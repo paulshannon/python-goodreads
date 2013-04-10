ed9a7c5f6e832c6d2f572ad1ea886d862bdf6873
Paul Shannon <github@paulshannon.ca>
2013-04-10 16:50:52 -0400

    auth example

---
+++ examples/user_authorization.py
@@ -0,0 +1,17 @@
+from config import DEVELOPER_KEY, DEVELOPER_SECRET
+from goodreads import Goodreads
+
+g = Goodreads(DEVELOPER_KEY, DEVELOPER_SECRET)
+
+authorize_url = g.oauth_authorize_url()
+
+print 'Please authorize at: %s' % authorize_url
+accepted = 'n'
+while accepted.lower() == 'n':
+    # you need to access the authorize_link via a browser,
+    # and proceed to manually authorize the consumer
+    accepted = raw_input('Have you authorized me? (y/n) ')
+
+token = g.oauth_retrieve_token()
+
+print 'You need to save key: \'%s\' and secret: \'%s\'' % (token.key, token.secret)
